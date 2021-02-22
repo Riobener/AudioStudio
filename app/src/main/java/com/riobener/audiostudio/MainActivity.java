a9c447c715d026ba357eac7cfa5d12d30e485e7d
Riobener <venisonsheff@gmail.com>
2021-02-22 22:36:04 +0300

    Moved to new c++ audio sdk by Superpowered.

--- app/src/main/java/com/riobener/audiostudio/MainActivity.java
+++ app/src/main/java/com/riobener/audiostudio/MainActivity.java
@@ -2,105 +2,113 @@ package com.riobener.audiostudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.AssetManager;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.SeekBar;
-import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
-    private AssetManager mgr;
-    RadioGroup radioGroup;
-    RadioButton sin;
-    RadioButton square;
-    SeekBar bpmChanger;
-    TextView bpmText;
-    int bpm = 120;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mgr = getResources().getAssets();
-        load(mgr);
-        setWaveType(1);
-        radioGroup = findViewById(R.id.waveChoosing);
-        sin = new RadioButton(this);
-        sin.setText("SIN");
-        sin.setOnClickListener(new View.OnClickListener() {
+
+        // Get the device's sample rate and buffer size to enable
+        // low-latency Android audio output, if available.
+        String samplerateString = null, buffersizeString = null;
+        AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        if (audioManager != null) {
+            samplerateString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            buffersizeString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+        }
+        if (samplerateString == null) samplerateString = "48000";
+        if (buffersizeString == null) buffersizeString = "480";
+        int samplerate = Integer.parseInt(samplerateString);
+        int buffersize = Integer.parseInt(buffersizeString);
+
+        // Files under res/raw are not zipped, just copied into the APK.
+        // Get the offset and length to know where our file is located.
+        AssetFileDescriptor fd = null;
+        try {
+            fd = getResources().getAssets().openFd("Sound.wav");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int fileOffset = (int)fd.getStartOffset();
+        int fileLength = (int)fd.getLength();
+        try {
+            fd.getParcelFileDescriptor().close();
+        } catch (IOException e) {
+            Log.e("PlayerExample", "Close error.");
+        }
+        String path = getPackageResourcePath();         // get path to APK package
+        NativeInit(samplerate, buffersize, getCacheDir().getAbsolutePath()); // start audio engine
+        OpenFileFromAPK(path, fileOffset, fileLength);  // open audio file from APK
+        // If the application crashes, please disable Instant Run under Build, Execution, Deployment in preferences.
+
+        // Update UI every 40 ms until UI_update returns with false.
+        Runnable runnable = new Runnable() {
             @Override
-            public void onClick(View v) {
-                setWaveType(1);
-                square.setChecked(false);
+            public void run() {
+                UI_update();
+                handler.postDelayed(this, 40);
             }
-        });
-        square = new RadioButton(this);
-        square.setText("SQUARE");
-        square.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setWaveType(2);
-                sin.setChecked(false);
-            }
-        });
-        radioGroup.addView(square);
-        radioGroup.addView(sin);
-
-
-        bpmChanger = (SeekBar)findViewById(R.id.bpmChanger);
-        bpmChanger.setProgress(bpm);
-        bpmChanger.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                setBpm(progress);
-                bpmText.setText(""+progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                bpmText.setText(""+bpmChanger.getProgress());
-            }
-        });
-
-        bpmText = (TextView)findViewById(R.id.bpmText);
-        bpmText.setText(""+bpm);
-
-
-
-        initialize();
-        Button playStop = findViewById(R.id.playStop);
-        playStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(soundState){
-                    soundState=false;
-                    playStop.setText("Stop");
-                    tap(true);
-                }else{
-                    soundState = true;
-                    playStop.setText("Start");
-                    tap(false);
-                }
-            }
-        });
+        };
+        handler = new Handler();
+        handler.postDelayed(runnable, 40);
     }
 
-    boolean soundState = true;
-    public native void initialize();
-    public native void tap(boolean i);
-    public native void setWaveType(int i);//1-sine, 2 - square
-    public native void setBpm(int bpm);
-    private static native void load(AssetManager mgr);
+    public void UI_update() {
+        boolean p = onUserInterfaceUpdate();
+        if (playing != p) {
+            playing = p;
+            Button b = findViewById(R.id.playPause);
+            b.setText(playing ? "Pause" : "Play");
+        }
+    }
+
+    // Play/Pause button event.
+    public void PlayerExample_PlayPause(View button) {
+        TogglePlayback();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        onBackground();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        onForeground();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Cleanup();
+    }
+    // Functions implemented in the native library.
+    private native void NativeInit(int samplerate, int buffersize, String tempPath);
+    private native void OpenFileFromAPK(String path, int offset, int length);
+    private native boolean onUserInterfaceUpdate();
+    private native void TogglePlayback();
+    private native void onForeground();
+    private native void onBackground();
+    private native void Cleanup();
+
+    private boolean playing = false;
+    private Handler handler;
 }
