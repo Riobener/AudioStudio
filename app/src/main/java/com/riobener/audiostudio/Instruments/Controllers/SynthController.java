4cd6bcc8632c1db25c218e7b09e3145aa750e0b6
Riobener <venisonsheff@gmail.com>
2021-05-09 22:17:57 +0300

    Added grid view for notes.

--- app/src/main/java/com/riobener/audiostudio/Instruments/Controllers/SynthController.java
+++ app/src/main/java/com/riobener/audiostudio/Instruments/Controllers/SynthController.java
@@ -1,7 +1,9 @@
 package com.riobener.audiostudio.Instruments.Controllers;
 
+import nl.igorski.mwengine.core.Filter;
 import nl.igorski.mwengine.core.RouteableOscillator;
 import nl.igorski.mwengine.core.SynthInstrument;
+import nl.igorski.mwengine.core.WaveForms;
 
 public class SynthController {
     /*SINE,
@@ -12,13 +14,25 @@ public class SynthController {
     PWM,
     KARPLUS_STRONG,
     TABLE*/
+    private Filter _filter;
     SynthInstrument synth;
     public SynthController(){
+        RouteableOscillator rosc = new RouteableOscillator();
+
+        rosc.setWave(4);
         synth = new SynthInstrument();
-        synth.setOscillatorAmount(2);
-        synth.getOscillatorProperties(0).setWaveform(0);
-        synth.getAdsr().setReleaseTime( 0.15f );
+        synth.setROsc(rosc);
         synth.getAudioChannel().setVolume(.7f);
+       //synth.getAdsr().setReleaseTime(0.3f);
+        synth.getAdsr().setDecayTime( .5f );
+        synth.getAdsr().setSustainLevel( 1f );
+        float maxFilterCutoff = ( float ) 48000 / 8;
+        float minFilterCutoff = 50.0f;
+        _filter = new Filter(
+                maxFilterCutoff / 2, ( float ) ( Math.sqrt( 1 ) / 2 ),
+                minFilterCutoff, maxFilterCutoff, 2
+        );
+        synth.getAudioChannel().getProcessingChain().addProcessor( _filter );
     }
 
     public SynthInstrument getSynth() {
